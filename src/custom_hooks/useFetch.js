ed1718782bee37ff48820e1f839dc8fe9c439285
ahoubouby <ahoubouby@gmail.com>
2020-06-15 22:12:20 +0100

    add loading, error handler to costum hooks

--- src/custom_hooks/useFetch.js
+++ src/custom_hooks/useFetch.js
@@ -2,15 +2,30 @@ import { useEffect, useState } from "react";
 
 export const useFetch = ({ url }) => {
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   useEffect(() => {
     const fetchData = async () => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((result) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        if (response.ok) {
           setData(result);
-        });
+        } else {
+          setHasError(true);
+          setErrorMessage(result);
+        }
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setHasError(true);
+        setErrorMessage(err);
+      }
     };
     fetchData();
   });
-  return [data];
+  return [data, isLoading, hasError, errorMessage];
 };
